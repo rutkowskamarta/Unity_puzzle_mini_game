63c56cae29dccfcdf009af911bde1042b16c2133
Marta Rutkowska <marta.rutkowska97@wp.pl>
2018-09-12 13:34:05 +0200

    randomizing and puzzle controller

---
+++ spoldzielnia-mini-game/Assets/Scripts/ElementBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementBehaviour : MonoBehaviour {
+
+    public int initialNumber;
+    public int currentIndex;
+  
+	void Start () {
+        currentIndex = initialNumber;
+        GetNeighbours();
+	}
+	
+	void Update () {
+
+    }
+
+    public bool CheckIfCorrectPosition()
+    {
+        return currentIndex == initialNumber;
+    }
+
+    public List<ElementBehaviour> GetNeighbours()
+    {
+        List<ElementBehaviour> neighbours = new List<ElementBehaviour>();
+        RaycastHit2D[] results= new RaycastHit2D[1];
+
+        BoxCollider2D boxCollider2D = GetComponent<BoxCollider2D>();
+        boxCollider2D.Raycast(Vector2.up, results, 2f);
+        if (results[0].collider != null) {
+            neighbours.Add(results[0].collider.gameObject.GetComponent<ElementBehaviour>());
+        }
+        boxCollider2D.Raycast(Vector2.right, results, 2f);
+        if (results[0].collider != null)
+        {
+            neighbours.Add(results[0].collider.gameObject.GetComponent<ElementBehaviour>());
+        }
+        boxCollider2D.Raycast(Vector2.down, results, 2f);
+        if (results[0].collider != null)
+        {
+            neighbours.Add(results[0].collider.gameObject.GetComponent<ElementBehaviour>());
+        }
+        boxCollider2D.Raycast(Vector2.left, results, 2f);
+        if (results[0].collider != null)
+        {
+            neighbours.Add(results[0].collider.gameObject.GetComponent<ElementBehaviour>());
+        }
+        return neighbours;
+    }
+
+    public void InitializeNumber(int number)
+    {
+        initialNumber = currentIndex = number;
+    }
+}
---
+++ spoldzielnia-mini-game/Assets/Scripts/PuzzleControler.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleControler : MonoBehaviour {
+
+    [SerializeField]private GameObject frame;
+    [SerializeField] private Sprite lockedFrame;
+    [SerializeField] private Sprite freeFrame;
+
+
+    private SpriteRenderer frameSpriteRenderer;
+    private List<Vector3> positions;
+    private List<Transform> puzzleElementsOrdered;
+    private ElementBehaviour removedElement;
+
+    private int indexOfCurrentPosition = 8;
+
+    private bool isHolding = false;
+    private bool isShuffled = false;
+    
+    private void Awake()
+    {
+        frameSpriteRenderer = frame.GetComponent<SpriteRenderer>();
+    }
+
+    void Update () {
+        if (isShuffled)
+        {
+            FrameControl();
+            HoldElement();
+        }
+	}
+
+    private void FrameControl()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            MoveUp();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            MoveLeft();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            MoveDown();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            MoveRight();
+        }
+
+    }
+
+    private void MoveLeft()
+    {
+        if(indexOfCurrentPosition!=0 && indexOfCurrentPosition != 3 && indexOfCurrentPosition != 6)
+        {
+            Vector3 newPosition = positions[indexOfCurrentPosition-1];
+            if (!isHolding)
+            {
+                frame.transform.position = new Vector3(newPosition.x, newPosition.y, frame.transform.position.z);
+            }
+            else
+            {
+                frame.transform.position = new Vector3(newPosition.x, newPosition.y, frame.transform.position.z);
+                MoveElementAtNewPosition(newPosition);
+            }
+            indexOfCurrentPosition--;
+        }
+
+    }
+    private void MoveRight()
+    {
+        if (indexOfCurrentPosition != 2 && indexOfCurrentPosition != 5 && indexOfCurrentPosition != 8)
+        {
+            Vector3 newPosition = positions[indexOfCurrentPosition + 1];
+            if (!isHolding)
+            {
+                frame.transform.position = new Vector3(newPosition.x, newPosition.y, frame.transform.position.z);
+            }
+            else
+            {
+                frame.transform.position = new Vector3(newPosition.x, newPosition.y, frame.transform.position.z);
+                MoveElementAtNewPosition(newPosition);
+            }
+            indexOfCurrentPosition++;
+        }
+
+    }
+    private void MoveUp()
+    {
+        if (indexOfCurrentPosition != 0 && indexOfCurrentPosition != 1 && indexOfCurrentPosition != 2)
+        {
+            Vector3 newPosition = positions[indexOfCurrentPosition-3];
+            if (!isHolding)
+            {
+                frame.transform.position = new Vector3(newPosition.x, newPosition.y, frame.transform.position.z);
+            }
+            else
+            {
+                frame.transform.position = new Vector3(newPosition.x, newPosition.y, frame.transform.position.z);
+                MoveElementAtNewPosition(newPosition);
+            }
+
+            indexOfCurrentPosition -= 3;
+        }
+
+    }
+    private void MoveDown()
+    {
+        if (indexOfCurrentPosition != 6 && indexOfCurrentPosition != 7 && indexOfCurrentPosition != 8)
+        {
+            Vector3 newPosition = positions[indexOfCurrentPosition+3];
+            if (!isHolding)
+            {
+                frame.transform.position = new Vector3(newPosition.x, newPosition.y, frame.transform.position.z);
+            }
+            else
+            {
+                frame.transform.position = new Vector3(newPosition.x, newPosition.y, frame.transform.position.z);
+                MoveElementAtNewPosition(newPosition);
+            }
+
+            indexOfCurrentPosition += 3;
+        }
+
+    }
+
+    private void MoveElementAtNewPosition(Vector3 newPosition)
+    {
+        ElementBehaviour heldElement = puzzleElementsOrdered[indexOfCurrentPosition].GetComponent<ElementBehaviour>();
+        if (heldElement != removedElement)
+        {
+            if (newPosition == removedElement.transform.position)
+            {
+                removedElement.transform.position = puzzleElementsOrdered[indexOfCurrentPosition].position;
+                removedElement.currentIndex = heldElement.currentIndex;
+                heldElement.transform.position = newPosition;
+                heldElement.currentIndex = indexOfCurrentPosition;
+                
+            }
+            
+        }
+    }
+
+    private void HoldElement()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isHolding || positions[indexOfCurrentPosition]  == removedElement.transform.position)
+            {
+                isHolding = false;
+                frameSpriteRenderer.sprite = freeFrame;
+            }
+            else
+            {
+                isHolding = true;
+                frameSpriteRenderer.sprite = lockedFrame;
+            }
+        }
+
+    }
+
+    public void InitializeController(List<Vector3> positions, List<Transform> puzzleElements, ElementBehaviour removedElement)
+    {
+        SetPositionList(positions);
+        SetPuzzleElementsList(puzzleElements);
+        SetRemovedElement(removedElement);
+        isShuffled = true;
+    }
+
+    public void SetPositionList(List<Vector3> positions)
+    {
+        this.positions = positions;
+    }
+
+    public void SetPuzzleElementsList(List<Transform> puzzleElements)
+    {
+        this.puzzleElementsOrdered = puzzleElements;
+    }
+
+    public void SetRemovedElement(ElementBehaviour removedElement)
+    {
+        this.removedElement = removedElement;
+    }
+
+}
